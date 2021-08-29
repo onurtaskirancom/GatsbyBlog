e60526e58baf12f778495b6ab06d831a90304577
onurtaskirancom <taskiranonur9@gmail.com>
2021-08-29 19:40:32 +0300

    some settings

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Link } from "gatsby";
+import "./Header.scss";
+
+export default function Header() {
+  return (
+    <div>
+      <h1 className='header-title'>I am Header</h1>
+      <Link to='/'>Home</Link>
+      <Link to='/about'>About</Link>
+      <Link to='/blogs'>Blogs</Link>
+      <Link to='/doesntexist'>Doesnt Exist</Link>
+    </div>
+  );
+}
---
+++ src/pages/faq.js
@@ -0,0 +1,10 @@
+import React from "react"
+
+export default function FAQ() {
+
+  return (
+    <div>
+      <h1 className="header-title">Hello FAQ</h1>
+    </div>
+  )
+}
