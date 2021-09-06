34c4f8511d18b5e496955fbcec40af6790528798
onurtaskirancom <taskiranonur9@gmail.com>
2021-09-06 14:44:23 +0300

    Styles & Templates

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,13 @@
-import React from "react";
-import { Link } from "gatsby";
-import "./Header.scss";
+import React from "react"
+import { Link } from "gatsby"
+
+import * as headetStyles from "./Header.module.scss"
 
 export default function Header() {
   return (
     <div>
-      <h1 className='header-title'>I am Header</h1>
+      <h1 className={headetStyles.headerTitle}>I am Header</h1>
+      <h2 className={headetStyles.headerSubtitle}>I am another Header</h2>
       <Link to='/'>Home</Link>
       <Link to='/about'>About</Link>
       <Link to='/blogs'>Blogs</Link>
